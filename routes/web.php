ae5a9df53e74bbaa85773dbb49ef04a97a54ac39
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2025-03-20 10:09:22 +0700

    MP composer & routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\UserDashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Halaman Login
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/', function () {
-    return view('welcome');
+// Middleware Auth
+Route::middleware(['auth'])->group(function () {
+    // Dashboard Admin (Hanya bisa diakses oleh Admin)
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])
+            ->name('admin.dashboard');
+    });
+
+    // Dashboard User (Hanya bisa diakses oleh User)
+    Route::middleware(['role:user'])->group(function () {
+        Route::get('/user/dashboard', [UserDashboardController::class, 'index'])
+            ->name('user.dashboard');
+    });
 });
